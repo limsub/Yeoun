f4c7b8d4932ee51eed2a54a4af99cab1e7ca067f
limsss9076@naver.com <limsss9076@naver.com>
2025-01-25 12:49:11 +0900

    Initial commit

---
+++ Yeoun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yeoun
+//
+//  Created by 임승섭 on 1/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
